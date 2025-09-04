78ab5034c40ad8748ae7f8efacdc08171895b0e9
ThembaGumede <gthemba82@gmail.com>
2025-09-04 14:51:14 +0200

    first commit

---
+++ EmployeeMS/src/Components/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import Login from "./Login.jsx";
+function App() {
+  return (
+    <>
+      <Login />
+    </>
+  );
+}
+
+export default App;
---
+++ EmployeeMS/src/Components/Login.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import "./style.css";
+import { useState } from "react";
+import axios from "axios";
+export const Login = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3000/auth/adminlogin ", values) // we are routing to the backend server
+      .then((result) => console.log(result))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
+      <div className="p-3 rounded w-25 border loginForm">
+        <h2>Login Page</h2>
+
+        <form onSubmit={handleSubmit}>
+          <div classname="mb-3">
+            <label htmlFor="email">
+              <strong>Email:</strong>
+            </label>
+            <input
+              type="email"
+              name="email"
+              autoComplete="off"
+              placeholder="Enter Email"
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
+              className="form-control rounded-0"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password:</strong>
+            </label>
+            <input
+              type="password"
+              name="password"
+              autoComplete="off"
+              placeholder="Enter Password"
+              onChange={(e) =>
+                setValues({ ...values, password: e.target.value })
+              }
+              className="form-control rounded-0"
+            />
+          </div>
+          <button className="btn btn-success w-100 rounded-0 mb-2">
+            Log in
+          </button>
+
+          <div className="mb-1">
+            <input type="checkbox" name="tick" className="me-2" />
+            <label htmlFor="password">
+              You agree with terms and conditions
+            </label>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default Login;
